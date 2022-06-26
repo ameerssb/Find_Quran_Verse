1845398b15d5fdfa3430cac74885b953a1573f3c
Sani Bature Sufyan <sanisbature17@gmail.com>
2022-06-26 14:53:27 +0100

    Update views.py

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -8,6 +8,7 @@ import mysql.connector
 import speech_recognition as sr
 from pydub import AudioSegment
 import requests
+import sys
 import json
 import re
 pd.options.mode.chained_assignment = None
@@ -28,6 +29,7 @@ def hello(req):
         if Aud != None:
             filename = Aud.name
             point = Changin2Wav(filename)
+            print(filename)    
             if point == 0:
                 return HttpResponse("Can't Process this type of file")
             try:
@@ -59,7 +61,7 @@ def hello(req):
                     sound = AudioSegment.from_file(Aud, '3gp')
                     sound.export(Aud, format='wav')
             except:
-                content = {"An error occured while reading this file, please check this file or upload another"}
+                content = {"An error occured while reading this file, please check this file or upload another" + sys.exc_info()[0]}
                 return HttpResponse(content, status=500)
 #            fs = FileSystemStorage()
 #            filename = fs.save(img_file.name, img_file)
@@ -138,6 +140,7 @@ def AndroidAudio(req):
         if Aud != None:
             filename = Aud.name
             point = Changin2Wav(filename)
+            print(filename)
             if point == 0:
                 return HttpResponse("Can't Process this type of file")
             try:
@@ -169,7 +172,8 @@ def AndroidAudio(req):
                     sound = AudioSegment.from_file(Aud, '3gp')
                     sound.export(Aud, format='wav')
             except:
-                content = {"An error occured while reading this file, please check this file or upload another"}
+                content = {"An error occured while reading this file, please check this file or upload another " + filename}
+                print(filename)
                 return HttpResponse(content, status=500)
 #            fs = FileSystemStorage()
 #            filename = fs.save(img_file.name, img_file)
@@ -390,103 +394,5 @@ def Processing(s_result):
                     answer = answer.iloc[:add]
                     verses = answer.to_json(force_ascii=False, orient="index")
                     return verses
-                    all_arr = verse_find[0]
-                    count = []
-                    data = {}
-                    deta = {}
-                    wordst = []
-                    words_found = {}
-                    words_found_all = {}
-                    c = 0
-                    zz = 0
-                    zs = 0
-                    cx = []
-                    for k in range(len(Speech_Text)):
-                        val = Speech_Text[k].split()
-                        for m in range(len(all_arr['text'])):
-                            cm = {}
-                            cc = 0
-                            ccc = 0
-                            cccc = 0
-                            val = Speech_Text[k].split()
-                            val2= all_arr['text'][m].split()
-                            for l in range(len(val)):
-                                for j in range(len(val2)):
-                                    cd = val[l]
-                                    dd = val2[j]
-                                    if val2[j] == val[l]:
-                                        if j+1 < len(val2) and l+1 < len(val):
-                                            if (val2[j+1] == val[l+1]):
-                                                cc+=1
-                                                if l ==0:
-                                                    zz+=1
-                                                wordst.append(val[l])
-                                        elif (j+1 >= len(val2) and l+1 >= len(val)):
-                                            ccc+=1
-                                            if l ==0:
-                                                zz+=1
-                                            wordst.append(val[l])
-                                    elif ('ا' in cd and 'أ|إ' in dd) and (len(cd) == len(dd)):
-                                        for i in range(len(cd)):
-                                            if (cd[i] =='ا' and dd[i] == 'أ|إ'):
-                                                cccc+=1
-                                                if l ==0:
-                                                    zs+=1
-                                                wordst.append(val[l])
-                            llm = cc + ccc+ cccc
-                            cx.append(llm)
-                            cc = 0
-                            cccc = 0
-                            words_found[m] = wordst
-                        words_found_all[k] = words_found
-                        for t in range(len(all_arr)):
-                            for i in range(len(val)):
-                                if i == 0:
-                                    ck = val[i] + " "
-                                elif i == (len(val)-1):
-                                    ck = " " + val[i]
-                                else:
-                                    ck = " " + val[i] + " "
-
-                                if ck in all_arr['text'][t]:
-                                    c+=1
-                            count.append(c)
-                            deta[t] = [t+1,cx[t],count[t],all_arr['text'][t],all_arr['score'][t],all_arr['sura'][t],all_arr['aya'][t],all_arr['Sura_Name'][t]]
-                            c = 0
-                        data[k] = deta
-                    Speech_Text_Rotate = len(data)
-                    row = len(data[0])
-                    col = len(data[0][0])
-                    dat = [[[data[0][j][i] for i in range(col)] for j in range(row)] for k in range(Speech_Text_Rotate)]
-                    frame = []
-                    all_data = {}
-                    for i in range(len(dat)):
-                        all_data[i] = pd.DataFrame(dat[i], columns = ['Suggestion','Number of Words in other','Word Count','text','Score','sura','aya','Sura_Name'])
-                    for i in range(len(dat)):
-                        frame.append(all_data[i])
-                    all_data = pd.concat(frame , keys=['S/N'])
-                    #        print(all_data['Number of Words in other'][0])
-                    #        print(all_data['Word Count'][0])
-                    all_data = all_data.sort_values(['Number of Words in other','Score','Word Count','text'], ascending = False)
-                    answer = all_data.drop(['Suggestion','Number of Words in other','Word Count','Score'], axis = 1)
-                    answer.index= np.arange(1, len(answer) + 1)
-                    answer = answer[['Sura_Name','sura','aya','text']]
-                    verses = answer.to_json(force_ascii=False, orient="index")
-                    print(answer)
-                    return verses
-                    for i in range(len(answer)):
-                        if answer['text'][i] in answer['text'][0]:
-                            if i == 0:
-                                ans[3] = answer['text'][i]
-                                ans[0] = answer['Sura_Name'][i]
-                                ans[1] = str(answer['sura'][i])
-                                ans[2] = str(answer['aya'][i])
-                                sani = sani + "Arabic Text: {3}\nSura Name: {0}\tSura Chapter: {1}\tAya: {2}\n".format(ans[0],ans[1],ans[2],ans[3])
-                            else:
-                                ans[0] = answer['Sura_Name'][i]
-                                ans[1] = str(answer['sura'][i])
-                                ans[2] = str(answer['aya'][i])
-                                sani = sani + "Sura Name: {0}\tSura Chapter: {1}\tAya: {2}\n".format(ans[0],ans[1],ans[2],ans[3])
-                    return sani
                 except KeyError:
                     return "Cant Find, Make sure you are reciting correctly"
