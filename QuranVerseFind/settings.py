c30f50bcef22bd7d0d32f329bf7ced1b93746826
Sani Bature Sufyan <sanisbature17@gmail.com>
2023-05-07 15:31:49 +0100

    static-files added

--- QuranVerseFind/settings.py
+++ QuranVerseFind/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1234567890'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','172.20.10.10']
+ALLOWED_HOSTS = ['quran.pythonanywhere.com','172.20.10.10','127.0.0.1']
 
 
 # Application definition
@@ -124,11 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static-files"
+
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [
+        BASE_DIR / "static"
+]
+
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ static-files/bootstrap/js/app.js
@@ -0,0 +1,297 @@
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
+
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var input; 							//MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext //audio context to help us record
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+var pauseButton = document.getElementById("pauseButton");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+pauseButton.addEventListener("click", pauseRecording);
+
+
+
+function startRecording() {
+	console.log("recordButton clicked");
+
+	/*
+		Simple constraints object, for more advanced audio features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
+    
+    var constraints = { audio: true, video:false }
+
+ 	/*
+    	Disable the record button until we get a success or fail from getUserMedia() 
+	*/
+
+	recordButton.disabled = true;
+	stopButton.disabled = false;
+	pauseButton.disabled = false
+
+	/*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
+
+		*/
+		audioContext = new AudioContext();
+
+
+		/*  assign to gumStream for later use  */
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+
+		/* 
+			Create the Recorder object and configure to record mono sound (1 channel)
+			Recording 2 channels  will double the file size
+		*/
+		rec = new Recorder(input,{numChannels:1})
+
+		//start the recording process
+		rec.record()
+
+		console.log("Recording started");
+
+	}).catch(function(err) {
+	  	//enable the record button if getUserMedia() fails
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+    	pauseButton.disabled = true
+	});
+}
+
+function pauseRecording(){
+	console.log("pauseButton clicked rec.recording=",rec.recording );
+	if (rec.recording){
+		//pause
+		rec.stop();
+		pauseButton.innerHTML="Resume";
+	}else{
+		//resume
+		rec.record()
+		pauseButton.innerHTML="Pause";
+
+	}
+}
+
+function stopRecording() {
+	console.log("stopButton clicked");
+
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = true;
+	pauseButton.disabled = true;
+
+	//reset button just in case the recording is stopped while paused
+	pauseButton.innerHTML="Pause";
+	
+	//tell the recorder to stop the recording
+	rec.stop();
+
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	//create the wav blob and pass it on to createDownloadLink
+	rec.exportWAV(createDownloadLink);
+}
+
+
+function createDownloadLink(blob) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toISOString();
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+	//add the new audio element to li
+	li.appendChild(au);
+		
+	filename = filename + ".wav";
+	//upload link
+//	var upload = document.createElement('a');
+//	upload.href="#";
+//	upload.onclick(search(blob,filename));
+//	upload.innerHTML = "Find Verse Again";
+	search(blob, filename);
+	recordButton.disabled = false;	
+	li.appendChild(document.createTextNode (" "))//add a space in between
+//	li.appendChild(upload)//add the upload link to li
+
+	//add the li element to the ol
+	recordingsList.appendChild(li);
+}
+
+function search(blob, filename){
+	var form=new FormData();
+		  form.append("file",blob, filename);
+		  $.ajax({
+			url: "/hello",
+			method: "POST",
+			processData: false,
+			mimeType: "multipart/form-data",
+			contentType: false,
+			data: form,
+			statusCode: {
+				404: function(responseObject, textStatus, jqXHR) {
+					alert('not found '+textStatus);
+					// No content found (404)
+					// This code will be executed if the server returns a 404 response
+				},
+				400: function(responseObject, textStatus, jqXHR) {
+					alert('Bad Request '+textStatus);
+					// No content found (404)
+					// This code will be executed if the server returns a 404 response
+				},
+				403: function(responseObject, textStatus, jqXHR) {
+					alert('Forbidden '+textStatus);
+					// No content found (404)
+					// This code will be executed if the server returns a 404 response
+				},
+				500: function(responseObject, textStatus, jqXHR) {
+					alert('Server Error '+textStatus);
+					// No content found (404)
+					// This code will be executed if the server returns a 404 response
+				},
+				503: function(responseObject, textStatus, errorThrown) {
+					alert("unavailable "+textStatus);
+					// Service Unavailable (503)
+					// This code will be executed if the server returns a 503 response
+				}},
+				success: function (data, statusCode){
+					var x = JSON.parse(data)
+					alert(`Sura Chapter: ${x.sura[0]} \nSura Name: ${x.Sura_Name[0]} \nVerse Number: ${x.aya[0]} \nArabic Text: ${x.text[0]}`)
+			},
+			  async: false,
+			  cache: false,
+			  timeout: 100000           
+		  });
+}
+
+function searchfind (){
+	var button = document.getElementById("audiobutton");
+	button.disabled = true; 
+    var form = new FormData();
+    var files = document.getElementById("file").files;
+    var file = files[0];
+    form.append("file", file, file.name);
+                $.ajax({
+                url: "/hello",
+                method: "POST",
+                processData: false,
+                mimeType: "multipart/form-data",
+                contentType: false,
+                data: form,
+				statusCode: {
+					404: function(responseObject, textStatus, jqXHR) {
+						alert('not found '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					400: function(responseObject, textStatus, jqXHR) {
+						alert('Bad Request '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					403: function(responseObject, textStatus, jqXHR) {
+						alert('Forbidden '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					500: function(responseObject, textStatus, jqXHR) {
+						alert('Server Error '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					503: function(responseObject, textStatus, errorThrown) {
+						alert("unavailable "+textStatus);
+						// Service Unavailable (503)
+						// This code will be executed if the server returns a 503 response
+					}},
+					success: function (data, statusCode){
+						var x = JSON.parse(data)
+						alert(`Sura Chapter: ${x.sura[0]} \nSura Name: ${x.Sura_Name[0]} \nVerse Number: ${x.aya[0]} \nArabic Text: ${x.text[0]}`)
+					},
+                async: false,
+                cache: false,
+                timeout: 30000
+			});
+		button.disabled = false; 
+    }
+
+
+function searchfindText (){
+		var button = document.getElementById("textbutton");
+		button.disabled = true; 
+		var form = new FormData();
+		form.append("arabic", $('#Text').val());
+					$.ajax({
+					url: "/translator",
+					method: "POST",
+					processData: false,
+					mimeType: "multipart/form-data",
+					contentType: false,
+					data: form,
+					statusCode: {
+					404: function(responseObject, textStatus, jqXHR) {
+						alert('not found '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					400: function(responseObject, textStatus, jqXHR) {
+						alert('Bad Request '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					403: function(responseObject, textStatus, jqXHR) {
+						alert('Forbidden '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					500: function(responseObject, textStatus, jqXHR) {
+						alert('Server Error '+textStatus);
+						// No content found (404)
+						// This code will be executed if the server returns a 404 response
+					},
+					503: function(responseObject, textStatus, errorThrown) {
+						alert("unavailable "+textStatus);
+						// Service Unavailable (503)
+						// This code will be executed if the server returns a 503 response
+					}},
+					success: function (data, statusCode){
+						var x = JSON.parse(data)
+						alert(`Sura Chapter: ${x.sura[0]} \nSura Name: ${x.Sura_Name[0]} \nVerse Number: ${x.aya[0]} \nArabic Text: ${x.text[0]}`)
+					},
+					async: false,
+					cache: false,
+					timeout: 100000           
+				});
+			button.disabled = false; 
+		}
+	
